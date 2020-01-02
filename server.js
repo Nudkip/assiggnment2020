fa943ea5f24cdf4d116b754d310f7fae4025b3c2
Nudkip <42031840+Nudkip@users.noreply.github.com>
2020-01-03 07:31:11 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,80 @@
+const fs = require('fs');
+const formidable = require('formidable');
+const express = require('express');
+const app = express();
+const ExifImage = require('exif').ExifImage;
+
+app.set('view engine','ejs');
+
+app.get('/', (req,res) => {
+	res.redirect('/filetoupload');
+});
+
+app.get('/filetoupload', (req,res) => {
+	res.status(200).render('filetoupload');
+});
+
+app.post('/filetoupload' , (req,res) => {
+	const form = new formidable.IncomingForm();
+	form.parse(req, (err, fields, files) => {
+		let title = null;
+		let description = null;
+		let image = null;
+		let make = null;
+		let model = null;
+		let createTime = null;
+		let location = null;
+		let dla = null;
+		let mla = null;
+		let sla = null;
+		let dlo = null;
+		let mlo = null;
+		let slo = null;
+		let lat = null;
+		let lon = null;
+		let iw = null;
+		let ih = null;
+		title = fields.title;
+        	description = fields.description;
+		console.log("1");
+		fs.readFile(files.filetoupload.path, (err,data) => {
+			image = new Buffer.from(data).toString('base64');
+			try {
+    				new ExifImage({ image : files.filetoupload.path }, function (error, exifData) {
+        				if (error){
+            					console.log('Error: '+error.message);
+						res.redirect('/error');
+					}else{
+						console.log(exifData);
+						make = exifData.image.Make; 
+						model = exifData.image.Model;
+						createTime = exifData.exif.CreateDate;
+						iw =  exifData.image.ImageWidth;
+						ih =  exifData.image.ImageHeight;
+						dla = exifData.gps.GPSLatitude[0];
+						mla = exifData.gps.GPSLatitude[1];
+						sla = exifData.gps.GPSLatitude[2];
+						lat = dla + mla/60 + sla/3600;
+						dlo = exifData.gps.GPSLongitude[0];
+						mlo = exifData.gps.GPSLongitude[1];
+						slo = exifData.gps.GPSLongitude[2];
+						lon = dlo + mlo/60 + slo/3600;
+						res.status(200).render('display', {t :title, d :description, i: image, ma: make, mo: model, c: createTime, ih: ih, iw: iw});
+						app.get('/map', (req,res) => {
+							res.status(200).render('map' ,{la: lat, lo: lon});
+						}); 
+					}
+    				});
+			} catch (error) {
+   				console.log('Error: ' + error.message);
+				res.redirect('/error');
+			}
+		});
+	});
+});
+
+app.get('/error', (req,res) => {
+	res.status(200).render('error');
+});
+
+app.listen(process.env.PORT || 8099);
